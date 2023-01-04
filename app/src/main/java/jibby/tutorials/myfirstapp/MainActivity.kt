1ec59b95d65cf80cac9d3770d70b15ca6297e78d
John Deen Bucog <JDBucog@yahoo.com>
2023-01-04 15:52:03 -0800

    counter done!

--- app/src/main/java/jibby/tutorials/myfirstapp/MainActivity.kt
+++ app/src/main/java/jibby/tutorials/myfirstapp/MainActivity.kt
@@ -2,10 +2,29 @@ package jibby.tutorials.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.btnClickMe)
+        val myText = findViewById<TextView>(R.id.tvText)
+        var counter = 0
+        btnClickMe.setOnClickListener {
+            if(counter > 10) {
+                Toast.makeText(this, "You're clicking too much!", Toast.LENGTH_LONG).show()
+                myText.text = "Stop!!! You're clicking too much!"
+                return@setOnClickListener
+            }
+            counter+=1
+            btnClickMe.text = "HAHA YOU CLICKED ME!"
+            //Alternatively...
+//            myText.text = "$counter"
+            myText.text = counter.toString()
+        }
     }
 }
